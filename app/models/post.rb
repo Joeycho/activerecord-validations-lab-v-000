57ef4e4d8c7699e0db530d06c26c15cb3de1455a
Joey Cho <cjy12182000@naver.com>
2019-02-18 10:29:44 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,8 +1,9 @@
 require 'pry'
 class MyValidator < ActiveModel::Validator
-  binding.pry
+
   #{ in: %w(Won't Believe Secret Top[number] Guess) }
   def validate(record)
+      binding.pry
     unless record.title.include? "Won't Believe" || "Secret" || "Top[number]" || "Guess"
       record.errors[:title] << 'title error'
     end
