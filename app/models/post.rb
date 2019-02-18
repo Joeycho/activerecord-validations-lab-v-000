06d50a7624796ee5675a5921a10061487ba68ca0
Joey Cho <cjy12182000@naver.com>
2019-02-18 10:25:58 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+  include ActiveModel::Validations
+  validates_with MyValidator
+  
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
@@ -7,9 +10,9 @@ end
 
 
 class MyValidator < ActiveModel::Validator
-  validates :title, inclusion: { in: %w(Won't Believe Secret Top[number] Guess) }
+  #{ in: %w(Won't Believe Secret Top[number] Guess) }
   def validate(record)
-    unless record.name.starts_with? 'X'
+    unless record.title.include? "Won't Believe" | "Secret" | "Top[number]" | "Guess"
       record.errors[:name] << 'Need a name starting with X please!'
     end
   end
